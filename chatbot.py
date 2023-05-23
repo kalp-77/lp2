361de8b86f593acd253b92b8e56f981010d73d5e
Kalp <90538362+kalp-77@users.noreply.github.com>
2023-05-23 13:47:42 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,9 @@
 from nltk.chat.util import Chat, reflections
 
 pairs = [
-    [r"my name is (.)", ["Hello %1, How are you"]]
+    [r"my name is (.*)", ["Hello %1, How are you"]],
+    [r"hey|hola|hi", ["hi how are you?"]]
 ]
 
-print("Hey there! I am Robot 2.0 at your service")
 chat = Chat(pairs, reflections)
 chat.converse()
