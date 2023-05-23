a3cd6bf4ba4697f1a2612a0eadf1810ebe13ea90
Kalp <90538362+kalp-77@users.noreply.github.com>
2023-05-23 19:13:09 +0530

    Update dijkstras.cpp

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -1,82 +1,92 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
-// void init_code() {
-// #ifndef ONLINE_JUDGE
-// 	freopen("input.txt", "r", stdin);
-// 	freopen("output.txt", "w", stdout);
-// #endif
-// }
 
 
-const int INF = INT_MAX;
-void printpath(vector<int>parent, int dest) {
-	if (parent[dest] == -1) {
-		cout << "No path from src to dest";
-		return;
-	}
-	vector<int>path;
-	for (int curr = dest; curr != -1; curr = parent[curr]) {
-		path.push_back(curr);
-	}
-	reverse(path.begin(), path.end());
+void printPath(vector<vector<pair<int,int>>>&graph, vector<int>&parent, int dest) {
 
-	cout << "Shortest Path : ";
-	for (int node : path) {
-		cout << node << " ";
-	}
-	cout << endl;
+    if(parent[dest] == -1) {
+        cout<<"no path exist";
+    }
+    vector<int>path;
+    for(int curr = dest; curr!=-1; curr = parent[curr]) {
+        path.push_back(curr);
+    }
+    reverse(path.begin(), path.end());
+    for(auto i: path) {
+        cout<<i <<" ";
+    }
+    cout<<endl;
 }
+void dij(vector<vector<pair<int,int>>>& graph, int src, int dest, int n) {
+    vector<int>dist(n+1, INT_MAX);
+    vector<int>parent(n+1, -1);
+    vector<int>vis(n+1);
 
-void dij(vector<vector<pair<int, int>>>& graph, int src, int dest, int n) {
-	vector<int>dist(n + 1, INF);
-	vector<int>parent(n + 1, -1);
-	vector<int>vis(n + 1, 0);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    dist[src] = 0;
+    pq.push({0, src});
+    while(!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
 
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	// {wt, node}
-	pq.push({0, src});
-	dist[src] = 0;
-	while (!pq.empty()) {
-		int node = pq.top().second;
-		pq.pop();
+        if(vis[u]) continue;
+        vis[u] = 1;
 
-		if (vis[node]) continue;
-		vis[node] = 1;
+        for(int i=0; i<graph[u].size(); i++) {
+            int v = graph[u][i].first;
+            int wt = graph[u][i].second;
 
-		for (int i = 0; i < graph[node].size(); i++) {
-
-			int child = graph[node][i].first;
-			int wt = graph[node][i].second;
-
-			if (dist[node] + wt < dist[child] && !vis[child]) {
-				dist[child] = dist[node] + wt;
-				pq.push({dist[child], child});
-				parent[child] = node;
-			}
-		}
-	}
-	printpath(parent, dest);
-
+            if(dist[u] + wt < dist[v] && !vis[v]){
+                dist[v] = dist[u] + wt;
+                pq.push({dist[v], v});
+                parent[v] = u;
+            }
+        }
+    }
+    printPath(graph, parent, dest);
+    int cost = 0;
+    cout << "Minimum cost from source to each vertex:\n";
+    for (int i = 1; i <= n; i++) {
+        cout << "Vertex " << i << ": ";
+        if (dist[i] != INT_MAX){
+            cost+=dist[i];
+            cout << dist[i] << endl;
+        }
+        else cout << "Not reachable" << endl;
+    }
+    cout<<dist[dest]<<endl;
 }
-
 int main() {
-	//init_code();
-	int n, m;
-	cin >> n >> m;
-	vector<vector<pair<int, int>>> graph(n + 1);
-	for (int i = 0; i < m; i++) {
-		int u, v, wt;
-		cin >> u >> v >> wt;
-		graph[u].push_back({v, wt});
-		graph[v].push_back({u, wt});
-	}
 
-	int src, dest;
-	cin >> src >> dest;
+    int n,m;
+    cin>>n>>m;
+    
+    vector<vector<pair<int,int>>>graph(n+1);
+    for(int i=0; i<m; i++) {
+        int u,v,wt;
+        cin>>u>>v>>wt;
+        graph[u].push_back({v,wt});
+        graph[v].push_back({u,wt});
+    }
 
-	dij(graph, src, dest, n);
+    int src,dest;
+    cin>>src>>dest;
+    dij(graph, src, dest, n);
 
-	return 0;
+    return 0;
 }
+
+// input: 
+// 6 10
+// 1 2 9
+// 1 3 4
+// 2 3 2
+// 3 4 1
+// 3 5 6
+// 2 4 7
+// 2 5 3
+// 4 5 4
+// 4 6 8
+// 5 6 2
+// 1 6
